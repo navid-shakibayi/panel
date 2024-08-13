2a8b41e95da6da2a279aaa12c2a0a9b9738e9749
navid-shakibayi <naweed.sh1474@gmail.com>
2024-08-14 00:51:11 +0330

    update panel component create button hover style

--- src/Components/AdminPanel/Panel.jsx
+++ src/Components/AdminPanel/Panel.jsx
@@ -25,7 +25,7 @@ const Panel = () => {
             <PanelCategorySlider selectedCategory={selectedCategory} onSelectCategory={setSelectedCategory} />
             <div className='flex justify-center'>
                 <button
-                    className='flex gap-2 justify-center items-center bg-custom-color31 text-center p-2 rounded px-6 sm:px-20'
+                    className='flex gap-2 justify-center items-center bg-custom-color31 text-center p-2 rounded px-6 sm:px-20 active:bg-custom-color3 hover:bg-opacity-80'
                     onClick={() => navigate('/panel/create-new-item')}
                 >
                     <span className='border-2 border-dashed rounded-full w-5 h-5 flex items-center justify-center font-bold'>+</span>
