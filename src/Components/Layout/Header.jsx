805a77aee5acf3014fbde63555da2a32bf1ab59e
navid-shakibayi <naweed.sh1474@gmail.com>
2024-11-18 01:12:14 +0330

    header style to mach category slider

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -25,15 +25,21 @@ const Header = ({
 
     return <>
         {data && <>
-            <div className="flex justify-between items-center max-w-6xl mx-auto px-2 py-6 bg-custom-color1 shadow-2xl">
-                <Link to='/'>
-                    <h2 className="text-3xl font-medium">{data.attributes.businessName}</h2>
-                    <h3 className="text-sm text-custom-color2">{data.attributes.businessSubtitle}</h3>
-                </Link>
+            <div className="grid grid-cols-4 gap-2 max-w-6xl mx-auto">
+                <span className="bg-custom-color2 grid grid-cols-2">
+                    <div></div>
+                    <div className="border-b-4 border-custom-color1"></div>
+                </span>
+                <div className="col-span-3 flex justify-between items-center py-6 bg-custom-color1 shadow-2xl pe-2">
+                    <Link to='/'>
+                        <h2 className="text-3xl font-medium">{data.attributes.businessName}</h2>
+                        <h3 className="text-sm text-custom-color2">{data.attributes.businessSubtitle}</h3>
+                    </Link>
 
-                <Link to='/' className="w-12 bg-custom-color2 p-1 rounded-2xl">
-                    <Home />
-                </Link>
+                    <Link to='/' className="w-12 bg-custom-color2 p-1 rounded-2xl">
+                        <Home />
+                    </Link>
+                </div>
             </div>
         </>}
     </>
