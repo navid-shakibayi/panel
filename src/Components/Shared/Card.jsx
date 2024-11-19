e10924c458db795092ed99d13668487ca51971bc
navid-shakibayi <naweed.sh1474@gmail.com>
2024-11-19 11:53:37 +0330

    out of stock items style

--- src/Components/Shared/Card.jsx
+++ src/Components/Shared/Card.jsx
@@ -11,6 +11,12 @@ const Card = ({
         <>
             <div className="relative bg-custom-color12 w-full rounded-2xl flex gap-2 p-2 shadow shadow-custom-color12">
                 <span className="absolute top-[5px] right-[5px] bg-custom-color2 w-8 h-8 rounded-tr-xl before:relative after:absolute after:bottom-0 after:left-0 after:w-[31px] after:h-[31px] after:rounded-tr-xl after:bg-custom-color12"></span>
+
+                {instock && (
+                    <div className="absolute inset-0 bg-black bg-opacity-30 text-white rounded-md backdrop-blur-[1px]">
+                    </div>
+                )}
+
                 <div className="relative flex-shrink-0">
                     <img
                         src={src}
@@ -29,17 +35,26 @@ const Card = ({
 
                     <div className="flex justify-between gap-2">
                         <p className="underline decoration-custom-color2 decoration-[3px] underline-offset-4">{price}</p>
-                        <button className="bg-custom-color2 w-6 h-6 rounded-md font-bold text-lg">+</button>
+                        <button
+                            className={`z-10 ${instock
+                                ? "bg-custom-color12 w- cursor-not-allowed font-bold text-xs px-2 rounded"
+                                : "bg-custom-color2 text-white w-8 h-8 rounded-md"
+                                }`}
+                            disabled={instock}
+                        >
+                            {instock ? "تمام شد" : "+"}
+                        </button>
                     </div>
                 </div>
-                {instock && (
-                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white rounded-md backdrop-blur-[1px]">
-                        <span className="text-lg font-bold drop-shadow-xl bg-yellow-600 bg-opacity-50 w-full text-center">تمام شد</span>
-                    </div>
-                )}
             </div>
         </>
     );
 };
 
 export default Card;
+
+
+
+{/* <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white rounded-md backdrop-blur-[1px]">
+    <span className="text-lg font-bold drop-shadow-xl bg-yellow-600 bg-opacity-50 w-full text-center">تمام شد</span>
+</div> */}
